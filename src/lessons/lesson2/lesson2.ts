3364b439950b0f217fc47152719d0cb670971887
Denis Kulik <kulikdenis1996x@gmail.com>
2023-06-15 19:41:06 +0700

    solve tasks of lesson 2

--- src/lessons/lesson2/lesson2.ts
+++ src/lessons/lesson2/lesson2.ts
@@ -24,6 +24,14 @@ console.log('lesson 2');
 // Task 01
 // Реализовать функцию sum которая суммирует 2 числа следующим образом sum(3)(6) === 9
 
+const sum = (a: number) => {
+    return (b: number) => {
+        return a + b;
+    };
+};
+
+console.log(sum(3)(6));
+
 // Task 02
 // Реализовать функцию makeCounter которая работает следующим образом:
 // const counter = makeCounter();
@@ -33,6 +41,21 @@ console.log('lesson 2');
 // counter2(); // 1
 // counter(); // 3
 
+const makeCounter = () => {
+    let count = 0;
+
+    return () => {
+        return count += 1;
+    };
+};
+
+const counter = makeCounter();
+console.log(counter()); // 1
+console.log(counter()); // 2
+const counter2 = makeCounter();
+console.log(counter2()); // 1
+console.log(counter()); // 3
+
 // Task 03
 // Переписать функцию из Task 02 так, что бы она принимала число в качестве аргумента и это число было стартовым значением счетчика
 // и возвращала следующий объект методов:
@@ -40,6 +63,24 @@ console.log('lesson 2');
 // decrease: -1
 // reset: установить счетчик в 0;
 // set: установить счетчик в заданное значение;
+
+const makeCounterUpd = (initialValue: number) => {
+    let count = initialValue;
+
+    return {
+        increase: () => count += 1,
+        decrease: () => count -= 1,
+        reset: () => count = 0,
+        set: (value: number) => count = value,
+    };
+};
+
+const counter3 = makeCounterUpd(1);
+console.log(counter3.increase()); // 2
+console.log(counter3.decrease()); // 1
+console.log(counter3.reset()); // 0
+console.log(counter3.set(3)); // 3
+console.log(counter3.increase()); // 4
 
 // Task 04*
 // Реализовать функцию superSum которая принимает число в качестве аргумента, которое указывает на количество слагаемых
@@ -53,11 +94,125 @@ console.log('lesson 2');
 
 // P.S. типизируйте только аргументы, а при вызове функции используйте @ts-ignore
 
+const superSum = (n: number) => {
+    if (n === 0) return 0;
+    if (n === 1) return (num: number) => num;
+
+    let _arguments: number[] = [];
+
+    function helper(...args: number[]) {
+        _arguments = [ ..._arguments, ...args ];
+        if (_arguments.length >= n) {
+            _arguments.length = n;
+            return _arguments.reduce((acc, number) => acc + number);
+        } else {
+            return helper;
+        }
+    }
+
+    return helper;
+};
+
+// @ts-ignore
+console.log(superSum(0)); //0
+// @ts-ignore
+console.log(superSum(3)(2)(5)(3)); //10
+// @ts-ignore
+console.log(superSum(3)(2)(5, 3)); //10
+// @ts-ignore
+console.log(superSum(3)(2, 5, 3)); //10
+// @ts-ignore
+console.log(superSum(3)(2, 5)(3)); //10
+// @ts-ignore
+console.log(superSum(3)(2, 5)(3, 9)); //10
+
 // Task 05
 // решить все задачи по рекурсии которые даны в конце статьи https://learn.javascript.ru/recursion
+
+const sumTo = (n: number): number => {
+    if (n === 1) return 1;
+
+    return n + sumTo(n - 1);
+};
+
+console.log(sumTo(5)); // 15
+
+const factorial = (n: number): number => {
+    if (n === 1) return 1;
+
+    return n * factorial(n - 1);
+};
+
+console.log(factorial(5)); // 120
+
+const fib = (n: number): number => {
+    let a = 1;
+    let b = 1;
+
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+};
+
+console.log(fib(7)); // 13
+
+const list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+const printList = (list: any) => {
+    console.log(list.value);
+
+    if (list.next) printList(list.next);
+};
+
+printList(list);
+
+const printReverseList = (list: any) => {
+    if (list.next) printReverseList(list.next);
+
+    console.log(list.value);
+};
+
+printReverseList(list);
 
 // Task 06
 // написать функцию, которая повторяет функционал метода flat массива на всю глубину.
 
+const deepFlat = (arr: any[]) => {
+    let result: any[] = [];
+
+    for (let i = 0; i < arr.length; i += 1) {
+        if (Array.isArray(arr[i])) {
+            result = result.concat(deepFlat(arr[i]));
+        } else {
+            result.push(arr[i]);
+        }
+    }
+
+    return result;
+};
+
+const arr = [ 1, 2, [ 3, [ 4, 5 ] ], 6 ];
+const flattened = deepFlat(arr);
+
+console.log(flattened); // [1, 2, 3, 4, 5, 6]
+
 // just a plug
-export default () => {};
+
+
+export {};
